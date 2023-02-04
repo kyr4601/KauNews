162b1558f16495212b76dfb60cf3f12816613d0f
kyr4601 <jane4545@naver.com>
2023-02-04 22:32:05 +0900

    동적생성테이블 추가

--- news.js
+++ news.js
@@ -9,5 +9,61 @@ $(document).ready(function(){
       $(this).addClass('current');
       $("#"+tab_id).addClass('current');
     })
+
+    getTable();
   
   })
+
+
+
+
+function getTable() {
+    
+  $('#tableBody').empty();
+   
+  var param = { "datasets" : [
+    { "NAME" : "공지사항 제목1", "from" : "미래교육혁신원", "date" : "2023-01-23", "cnt" : 2234, "link": "https://lms.kau.ac.kr/login.php?errorcode=4" },
+    { "NAME" : "공지사항 제목2", "from" : "시설원", "date" : "2023-01-24", "cnt" : 123, "link": "https://lms.kau.ac.kr/login.php?errorcode=4"},
+    { "NAME" : "공지사항 제목3", "from" : "기타", "date" : "2023-01-25", "cnt" : 4523, "link": "https://lms.kau.ac.kr/login.php?errorcode=4" }
+  ]}
+  
+  for(var i = 0; i < param.datasets.length; i++) {
+    var tableTd = '<tr>';
+    tableTd += '<td>'+ '<a href="' + param.datasets[i].link + '">' + param.datasets[i].NAME +'</a>'+  '</td>';
+    tableTd += '<td class="tb-menu">' + param.datasets[i].from + '</td>';
+    tableTd += '<td>' + param.datasets[i].date + '</td>';
+    tableTd += '<td class="tb-menu">' + param.datasets[i].cnt + '</td>';
+    tableTd += '</tr>';
+
+  
+    $('#tableBody').append(tableTd);
+
+    
+  }
+    
+}
+
+
+function getTable2() {
+    
+  $('#tableBody2').empty();
+  
+  var param = { "datasets" : [
+    { "NAME" : "공지사항 제목1", "from" : "미래교육혁신원", "date" : "2023-01-23", "cnt" : 2234 },
+    { "NAME" : "공지사항 제목2", "from" : "시설원", "date" : "2023-01-24", "cnt" : 123},
+    { "NAME" : "공지사항 제목3", "from" : "기타", "date" : "2023-01-25", "cnt" : 321 }
+  ]}
+
+  for(var i = 0; i < param.datasets.length; i++) {
+    var tableTd = '<tr>';
+    tableTd += '<td>' + param.datasets[i].NAME + '</td>';
+    tableTd += '<td>' + param.datasets[i].from + '</td>';
+    tableTd += '<td>' + param.datasets[i].date + '</td>';
+    tableTd += '<td>' + param.datasets[i].cnt + '</td>';
+    tableTd += '</tr>';
+
+    $('#tableBody2').append(tableTd);
+  }
+  
+}
+
